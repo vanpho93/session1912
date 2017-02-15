30620c9cad1425f3b1a12c8c421ff19b1ed2352f
Khoa Pham <khoaphp@yahoo.com>
2017-02-15 20:28:08 +0700

    refresh

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ app.listen(3000, () => console.log('Server started'));
 app.use(session({
   secret: 'keyboard hvfdgh7657688cat',
   resave: false,
-  saveUninitialized: true
-}))
+  saveUninitialized: true,
+  cookie: {maxAge: 10000}
+}));
 
-app.get('/', (req, res) => res.send('Hello'))
+app.use((req, res, next) => {
+  req.name = 'KhoaPham';
+  next();
+})
+
+app.get('/', (req, res) => res.send('Hello ' + req.name))
 
 app.get('/muave', (req, res) => {
   req.session.daMuaVe = true;
@@ -18,6 +24,7 @@ app.get('/muave', (req, res) => {
 
 app.get('/vaorap', (req, res) => {
   if(req.session.daMuaVe){
+    req.session.count = req.session.count ? ++req.session.count : 1;
     return res.send('Welcome')
   }
   res.send('Ban phai mua ve truoc **********')
